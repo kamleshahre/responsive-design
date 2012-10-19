77a3241d3a7bdee3e27e2479c329e313383ea192
nolte <nolte07@googlemail.com>
2012-10-19 18:33:16 +0100

    Aspect Trace Logging Hinzugefügt

---
+++ noltarium/src/main/java/de/noltarium/page/aspect/LoggingAspect.java
@@ -0,0 +1,52 @@
+package de.noltarium.page.aspect;
+
+import java.lang.reflect.Field;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Aspect
+public class LoggingAspect {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+	
+	@Before("execution(* de.noltarium.page.controller..*.*(..))")
+	public void logBefore(JoinPoint joinPoint) {
+	
+		Logger logger = getLoggerFromJoinPoint(joinPoint);
+		logger.trace("{}(..) start",joinPoint.getSignature().getName());
+	}
+	
+	@After("execution(* de.noltarium.page.controller..*.*(..))")
+	public void logAfter(JoinPoint joinPoint) {
+	
+		Logger logger = getLoggerFromJoinPoint(joinPoint);
+		logger.trace("{}(..) end",joinPoint.getSignature().getName());
+	}
+	
+	private Logger getLoggerFromJoinPoint(JoinPoint joinPoint)
+	{
+		Class clazz = joinPoint.getSignature().getDeclaringType();
+		try {
+			Field loggerField = clazz.getDeclaredField("LOGGER");
+			loggerField.setAccessible(true);
+			Logger logger = (Logger) loggerField.get(null);
+			return logger;
+		} catch (SecurityException e) {
+			LOGGER.error("",e);
+		} catch (NoSuchFieldException e) {
+			LOGGER.error("",e);
+		} catch (IllegalArgumentException e) {
+			LOGGER.error("",e);
+		} catch (IllegalAccessException e) {
+			LOGGER.error("",e);
+		}
+		LOGGER.error("Logger not found using aspect logger");
+		return LOGGER;
+	}
+
+}
--- noltarium/src/main/java/de/noltarium/page/controller/WelcomController.java
+++ noltarium/src/main/java/de/noltarium/page/controller/WelcomController.java
@@ -1,6 +1,8 @@
 package de.noltarium.page.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/welcome")
 public class WelcomController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(WelcomController.class);
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
