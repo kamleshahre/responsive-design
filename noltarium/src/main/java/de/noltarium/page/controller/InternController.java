1961c16ab59eb2440e68ee818b9ace7cb4f5598f
nolte <nolte07@googlemail.com>
2012-10-19 20:43:06 +0100

    Spring security eingebaut

--- noltarium/src/main/java/de/noltarium/page/controller/InternController.java
+++ noltarium/src/main/java/de/noltarium/page/controller/InternController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/intern")
+@RequestMapping("/intern/main")
 public class InternController {
 
 	private static final Logger LOGGER = LoggerFactory
